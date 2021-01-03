d531687736b2a48f4a8232d5976652b19e1dd019
Martin Martinez <investingmtz@gmail.com>
2021-01-03 11:19:27 -0600

    Mostrando tablero por consola

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,3 +3,12 @@ board = {
     'mid-L': ' ', 'mid-M' : ' ', 'mid-R' : ' ',
     'low-L': ' ', 'low-M' : ' ', 'low-R' : ' ',
 }
+
+def printboard(board):
+    print(board['top-L'] + ' |' + board['top-M'] + ' |' + board['top-R'])
+    print('__+__+__')
+    print(board['mid-L'] + ' |' + board['mid-M'] + ' |' + board['mid-R'])
+    print('__+__+__')
+    print(board['low-L'] + ' |' + board['low-M'] + ' |' + board['low-R'])
+
+printboard(board)
