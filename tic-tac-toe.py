00b4f96bb3f97e8845b012946e78754b24ecb388
Martin Martinez <investingmtz@gmail.com>
2021-01-03 11:33:58 -0600

    Funcion para jugar

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -11,16 +11,16 @@ def printboard(board):
     print('__+__+__')
     print(board['low-L'] + ' |' + board['low-M'] + ' |' + board['low-R'])
 
-turn = 'X'
-
-for i in range(9):
+def play(turn):
+    for i in range(9):
+        printboard(the_board)
+        print('Turn for ' + turn + '. Move on which space?')
+        move = input()
+        the_board[move] = turn
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
     printboard(the_board)
-    print('Turn for ' + turn + '. Move on which space?')
-    move = input()
-    the_board[move] = turn
-    if turn == 'X':
-        turn = 'O'
-    else:
-        turn = 'X'
 
-printboard(the_board)
+play('X')
